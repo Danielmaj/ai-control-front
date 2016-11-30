4919779656e142f8da5ac407411887fe1f93df8c
liivur <sander.sats@gmail.com>
2016-11-30 15:16:46 +0200

    Added tsne mockup

--- src/app/landing/landing.component.ts
+++ src/app/landing/landing.component.ts
@@ -1,6 +1,10 @@
 import { Component } from '@angular/core';
 
+
 import { NetworkService } from '../services';
+import { BehaviorSubject } from 'rxjs/BehaviorSubject';
+import { Observable } from 'rxjs/Observable';
+import { Subscription } from 'rxjs/Subscription';
 
 // var helpers = require('../../../config/helpers');
 // console.log('helpers', helpers.root('src'));
@@ -10,6 +14,10 @@ import { NetworkService } from '../services';
 // console.log(__dirname);
 // var ROOT_FOLDER = path.resolve(__dirname, '..');
 // console.log(ROOT_FOLDER);
+
+// import '../../../public/js/tsne.js';
+
+declare var tsnejs: any;
 
 @Component({
   selector: 'landing',
@@ -23,12 +31,41 @@ export class LandingComponent {
 		world: {}
 	};
 	editWorld = false;
+	tsne: any;
+	dataObservable: BehaviorSubject<number[][]> = new BehaviorSubject<number[][]>([]);
+	points: number[][];
+	dists: number[][] = [];
+
+	fakeData: any[] = [];
+
+	side = 4;
+
+	color = '#000';
+	timer: Observable<any>;
+	speed = 10;
+	runner: Subscription;
+
+	tsneOptions = {
+		epsilon: 10, // epsilon is learning rate (10 = default)
+		perplexity: 10, // roughly how many neighbors each point influences (30 = default)
+		dim: 2, // dimensionality of the embedding (2 = default)
+	};
 
 	constructor(
 		private _networkService: NetworkService
-	) {}
+	) {
+	}
 
 	ngOnInit() {
+		this.generateData();
+		this.timer = Observable.timer(0,this.speed);
+		this.dataObservable.subscribe((value) => {
+			// console.log('observed', value);
+			this.points = value;
+		});
+
+		this.tsne = new tsnejs.tSNE(this.tsneOptions); // create a tSNE instance
+		console.log(this.tsne);
 		this.getWorld();
 	}
 
@@ -41,6 +78,86 @@ export class LandingComponent {
 			console.log(network);
 			this.network = network;
 		});
+	}
+
+	sleep(ms: number): Promise<any> {
+	  return new Promise(resolve => setTimeout(resolve, ms));
+	}
+
+	generateData(): void {
+		var data: any[][] = [];
+		// i%this.side + ', ' + Math.floor(i/this.side)
+		for (var i = 0; i < this.side; ++i) {
+			for (var j = 0; j < this.side; ++j) {
+				for (var k = 0; k < this.side; ++k) {
+					for (var l = 0; l < this.side; ++l) {
+						data.push([i, j, k, l]);
+					}
+				}
+			}
+			// data[i] = [i%this.side, Math.floor(i/this.side)]
+		}
+		this.fakeData = data;
+		var size = data.length;
+		var dists: number[][] = [];
+		for(var i = 0; i < size; i++) {
+			dists.push([]);
+			for(var j = 0; j < size; j++) {
+				dists[i].push(0);
+			}
+		}
+		// compute pairwise distances
+		for(var i = 0; i < size; i++) {
+			for(var j = i + 1; j < size; j++) {
+				dists[i][j] = 0
+				for (var k = data[i].length - 1; k >= 0; k--) {
+					dists[i][j] += Math.pow(data[i][k] - data[j][k], 2)
+				}
+				dists[i][j] = Math.sqrt(dists[i][j]) / 50;
+				dists[j][i] = dists[i][j];
+			}
+		}
+		this.dists = dists;
+	}
+
+	tsneTest() {
+		// initialize data. Here we have 3 points and some example pairwise dissimilarities
+		// var dists = [
+		// 	[1.0, 0.1, 0.2], [0.1, 1.0, 0.3], [0.2, 0.1, 1.0],
+		// ];
+		// var size = 100;		
+
+		this.tsne = new tsnejs.tSNE(this.tsneOptions); // create a tSNE instance
+		this.tsne.initDataDist(this.dists);
+
+		// this.data = this.tsne.getSolution(); // Y is an array of 2-D points that you can plot
+		// for(var k = 0; k < 5; k++) {
+		// 	// this.sleep(1000).then(() => this.tsneSteps(100));
+		// 	this.tsneSteps(500);
+		// }
+		this.runner = this.timer.subscribe(() => this.tsneSteps(1));
+
+		this.dataObservable.next(this.tsne.getSolution()); // Y is an array of 2-D points that you can plot
+	}
+
+	continueTsne(): void {
+		if (!this.runner || this.runner.closed) {
+			this.runner = this.timer.subscribe(() => this.tsneSteps(1));
+		}
+	}
+
+	stopTsne(): void {
+		if (this.runner) {
+			this.runner.unsubscribe();
+		}
+	}
+
+	tsneSteps(steps: number): void {
+		// console.log(this.tsne);
+		for(var k = 0; k < steps; k++) {
+			this.tsne.step(); // every time you call this, solution gets better
+		}
+		this.dataObservable.next(this.tsne.getSolution());
 	}
 
 	saveWorld() {
